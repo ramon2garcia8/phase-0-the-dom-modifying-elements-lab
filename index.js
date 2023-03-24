34aa8e83c1e659b5d3da383c69d5d8b7b78b92d5
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-03-23 20:02:48 -0400

    dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Ramon A. Garcia is the champion";
